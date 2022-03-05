695068bce9e384529bda3d9dcc318e595445efec
rpshende <rpshende@DELL-PRECISION5000>
2022-03-05 13:27:11 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD6m-9GWMSWfJiOoBcW9DUXdJiUlehKHtE",
+    authDomain: "e-ride-s4.firebaseapp.com",
+    projectId: "e-ride-s4",
+    storageBucket: "e-ride-s4.appspot.com",
+    messagingSenderId: "45437878521",
+    appId: "1:45437878521:web:10409e0b3462f01c16e228"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
